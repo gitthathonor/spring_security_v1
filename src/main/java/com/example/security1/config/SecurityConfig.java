3b9f1bf0ed297e1274df13f6c3b4219abb167e73
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-18 01:14:53 +0900

    시큐리티 @Secured로 권한처리 완료

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true ) // Secured 어노테이션 활성화, PreAuthorize, PostAuthorize 어노테이션 활성화
 public class SecurityConfig {
 	
 	
