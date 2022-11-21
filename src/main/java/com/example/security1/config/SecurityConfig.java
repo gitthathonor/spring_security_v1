1cb69be997eb9034e35bc34e2e31a2e45f730865
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-21 20:54:24 +0900

    구글 회원 프로필 정보 받아보기

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -1,5 +1,15 @@
 package com.example.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+// 1.코드받기(인증)
+// 2.엑세스토큰(권한) 
+// 3.사용자프로필 정보를 가져와서 
+// 4. 그 정보를 토대로 회원가입을 자동으로 진행시키키도 함
+// 4-2 (이메일, 전화번호, 이름, 아이디) => 그대로 구글 프로필에서 정보를 받아오면 됨
+// 쇼핑몰 -> (집주소), 백화점몰 -> (vip등급, 일반등급) => 새로운 회원가입 창이 필요하다.
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +18,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.security1.auth.oauth.PrincipalOauth2UserService;
+
 @Configuration
 @EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true ) // Secured 어노테이션 활성화, PreAuthorize, PostAuthorize 어노테이션 활성화
 public class SecurityConfig {
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	
 	
 	@Bean // 해당 메서드의 리턴되는 오브젝트를 IoC에 등록시켜주는 어노테이션
@@ -37,8 +52,11 @@ public class SecurityConfig {
 		.defaultSuccessUrl("/")
 		.and()
 		.oauth2Login()
-		.loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요함
-
+		.loginPage("/loginForm")
+		.userInfoEndpoint()
+		.userService(principalOauth2UserService); 
+		// 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드X, (엑세스토큰+사용자프로필정보 O)
+		// 이미 코드를 던져서 인증이 끝난 엑세스토큰과 사용자프로필 정보를 리턴한다.
 		return http.build();
 	}
 }
--- src/main/java/com/example/security1/model/Users.java
+++ src/main/java/com/example/security1/model/Users.java
@@ -23,7 +23,10 @@ public class Users {
 	private String username;
 	private String password;
 	private String email;
-	private String role;
+	private String role; // ROLE_USER, ROLE_ADMIN
+	
+	private String provider;
+	private String providerId;
 	@CreationTimestamp
 	private Timestamp createDate; 
 }
